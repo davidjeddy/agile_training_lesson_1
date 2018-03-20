68dac14921d6f444801cf3b026a596c9335a0f66
David J Eddy <me@davidjeddy.com>
2018-03-20 13:02:06 -0400

    Two tests running.

--- __tests__/ParseBowlingScore.js
+++ __tests__/ParseBowlingScore.js
@@ -18,17 +18,31 @@
  */
 
 class ParseBowlingScore {
+
+    constructor() {
+        this.gameData = '{"date":"2017-01-01","rolls":"11111111111111111111"}';
+    }
+
+    convertGameStringToObject() {
+        return JSON.parse(this.gameData);
+    }
+
     parseScore () {
         return 'asdf';
     }
 }
 
 describe( "Parse Bowling Score", function () {
+    const sut = new ParseBowlingScore();
 
     it ("entire score is a string", function () {
-      const sut = new ParseBowlingScore();
-      const result = sut.parseScore();
-      expect(typeof result).toBe('string')
+        const result = sut.parseScore();
+        expect(typeof result).toBe('string')
     });
 
+    it ("Convert string to Object", function () {
+        const result = sut.convertGameStringToObject();
+        expect(typeof result).toBe('object')
+        expect(result.date).toEqual('2017-01-01')
+    })
 });
