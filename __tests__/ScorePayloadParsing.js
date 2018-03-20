cb5f0d5d8d7e4c6bcf05dd246f077dbddd730e0b
David J Eddy <me@davidjeddy.com>
2018-03-20 12:34:09 -0400

    INIT constraints defined

---
+++ __tests__/ScorePayloadParsing.js
@@ -0,0 +1,19 @@
+// Strikes = 'X' = strike = 10, convert to integer of 10
+// Dash = '-' = Gutter = 0
+// no delimiters
+// String number {1-9} = integer 1 -> 9
+// Spare = '/' = prev. number subtract 10.
+
+// Given the string return an array of numbers:
+// Exp: 9/------------------ response would be
+/**
+ * [
+ *   9,
+ *   1,
+ *   0,
+ *   0,
+ *   0,
+ *   ...
+ * ]
+ */
+
