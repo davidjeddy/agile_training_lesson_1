5f24a9095899652efea5033173f0d225f3d33766
David J Eddy <me@davidjeddy.com>
2018-03-20 12:25:13 -0400

    Example two of TDD in Js using Jest in ES6 syntax

--- __tests__/GreetingBuilder.js
+++ __tests__/GreetingBuilder.js
@@ -1,15 +1,13 @@
 class GreetingBuilder {
     build ( name = "World" ) {
-        return "Hello, " + name + "!"
+        return "Hello, " + name + "!";
     }
 }
 
 describe( "Greeting Builder", function () {
     it ( "return a default message", function () {
         const sut = new GreetingBuilder();
-
         const result = sut.build();
-
         expect( result ).toEqual( "Hello, World!" )
     })
 });
