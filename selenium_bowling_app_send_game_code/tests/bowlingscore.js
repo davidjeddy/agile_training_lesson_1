710b72bc432ce6410f22fcf3b505c062e671fbed
David J Eddy <me@davidjeddy.com>
2018-03-21 15:18:36 -0400

    Selenium test testing login/submit form for bowlingscore.info

--- selenium_bowling_app_send_game_code/tests/bowlingscore.js
+++ selenium_bowling_app_send_game_code/tests/bowlingscore.js
@@ -23,12 +23,17 @@ describe( "Visit bowlingscore.info", function () {
     } )
 
     it( "visit bowlingscore.info using selenium + ChromeDriver + BrowserDriver.io", function () {
+
         return driver.url( url )
             .saveScreenshot('./tests/screenshots/1-0.png')
-            .click( "#button" )
+            .element('#user').setValue('Tracy')
+            .element('#pass').setValue('asdf1234')
+            .element('#ubc').setValue('1111')
+            .click("#button")
             .saveScreenshot('./tests/screenshots/1-1.png')
             .getTitle().then(
-                title => expect( title ).toEqual( "Bowling Score Info" ) )
+                title => expect( title ).toEqual( "Bowling Score Info" )
+            )
 
     })
 });
